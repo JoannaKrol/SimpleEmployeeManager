e794cb4792ab24b29c4fea3d8bb2c2705e1a270c
JoannaKrol <joannakrol942@gmail.com>
2024-09-11 00:48:42 +0200

    Change connection implementation

--- Backend/SimpleEmployeeManager/Data/EmployeesDbContext.cs
+++ Backend/SimpleEmployeeManager/Data/EmployeesDbContext.cs
@@ -23,15 +23,10 @@ namespace SimpleEmployeeManager.Data
                         v => (Sex)Enum.Parse(typeof(Sex), v)
                     );
                 em.ToTable(t => t.HasCheckConstraint("CK_Employees_Sex", "[Sex] IN ('M', 'F')"));
-                em.ToTable(t => t.HasCheckConstraint("CK_Employees_Age", "[Age] BEWTEEN 18 AND 100"));
+                em.ToTable(t => t.HasCheckConstraint("CK_Employees_Age", "[Age] BETWEEN 18 AND 100"));
             });
 
             base.OnModelCreating(modelBuilder);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=(localdb);Database=EmployeesDb;Trusted_Connection=True;");
         }
     }
 }
