8903040bedc295193a56e06197c2883ba0433f98
JoannaKrol <joannakrol942@gmail.com>
2024-09-11 23:20:04 +0200

    Configure initial data

--- Backend/SimpleEmployeeManager/Data/EmployeesDbContext.cs
+++ Backend/SimpleEmployeeManager/Data/EmployeesDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SimpleEmployeeManager.Entites;
 using SimpleEmployeeManager.Enums;
+using SimpleEmployeeManager.Generators;
 
 namespace SimpleEmployeeManager.Data
 {
@@ -19,11 +20,12 @@ namespace SimpleEmployeeManager.Data
                     .HasMaxLength(1)
                     .IsRequired()
                     .HasConversion(
-                        v => v.ToString(),
+                        v => ((char)v).ToString(),
                         v => (Sex)Enum.Parse(typeof(Sex), v)
                     );
                 em.ToTable(t => t.HasCheckConstraint("CK_Employees_Sex", "[Sex] IN ('M', 'F')"));
                 em.ToTable(t => t.HasCheckConstraint("CK_Employees_Age", "[Age] BETWEEN 18 AND 100"));
+                em.HasData(EmployeesDataGenerator.GenerateUsers(3));
             });
 
             base.OnModelCreating(modelBuilder);
---
+++ Backend/SimpleEmployeeManager/Generators/EmployeesDataGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using SimpleEmployeeManager.Entites;
+using SimpleEmployeeManager.Enums;
+
+namespace SimpleEmployeeManager.Generators
+{
+    public class EmployeesDataGenerator
+    {
+        public static List<Employee> GenerateUsers(int numberOfEmployees)
+        {
+            var userFaker = new Faker<Employee>()
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                .RuleFor(u => u.LastName, f => f.Name.LastName())
+                .RuleFor(u => u.Age, f => f.Random.Int(18, 100))
+                .RuleFor(u => u.Sex, f => f.PickRandom<Sex>());
+
+            return userFaker.Generate(numberOfEmployees);
+        }
+    }
+}
